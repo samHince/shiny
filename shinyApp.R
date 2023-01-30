c1c3901e27f812e7b50dacc4d47bbe4e44dc19de
samHince <sam.hince7@gmail.com>
2023-01-29 22:03:52 -0800

    Tried many things to make reactive shiny elements. Not yet working.

--- shinyApp.R
+++ shinyApp.R
@@ -14,7 +14,7 @@ ui <- fluidPage( #
                                 "Kilometers")))
       ),
       fluidRow(
-        column(3, textInput("time", "Time", "Time Run")),
+        column(3, textInput("time", "Time", 60)),
         column(2, selectInput("distUnits", "Units",
                               c("Minutes",
                                 "Hours")))
@@ -26,6 +26,7 @@ ui <- fluidPage( #
                                 "Min per Km")))
       ),
       # test 
+      "Pace:",
       textOutput("pace_out")
       # end test code
     )
@@ -33,14 +34,38 @@ ui <- fluidPage( #
 )
 
 # Define server logic ----
-server <- function(input, output) {
-  output$pace_out <- {(
-    renderText(input$time)
-  )}
+server <- function(input, output, session) {
+  values <- reactiveValues(pace = 1, time = 1, dist = 10) #reactive({input$time})
+  
+  #values$time <- reactive({input$time()})
+  
+  #observeEvent(input$time, {
+  #  values$pace <- values$dist / values$time
+  #})
+  
+  #observe(input$time, {
+  #values$time <- reactive({input$time})
+  #values$pace <- reactive({values$dist}) / reactive({values$time})
+  #})
+  
+  #observe(input$dist, {
+  #  values$dist <- reactive({input$dist})
+  #  values$pace <- reactive({values$dist / values$time})
+  #})
+  caldpace <- reactive({
+    as.numeric(input$time)
+    #input$dist / input$time
+  })
+  
+  output$pace_out <- renderText({
+    #as.numeric(input$time)
+    caldpace
+    #input$dist / input$time
+    #values$time
+  })
 }
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
 
 #heart rate: 220-age = max ... max - resting * % + resting (1 - 50-60%, 60-70%)
-#
